5be059d4852135b5895c251d9426a5a01c3673cd
lexaeliseev <lexaeliseev@gmail.com>
2024-11-02 21:58:17 +0300

    add fixture firefox_headless_mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,13 +6,17 @@ from selenium import webdriver
 @pytest.fixture(autouse=True)
 def browser_settings():
     browser.config.base_url = 'https://github.com'
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
-
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
     yield
 
     browser.quit()
+
+
+@pytest.fixture()
+def firefox_headless_mode():
+    driver_options = webdriver.FirefoxOptions()
+    driver_options.page_load_strategy = 'eager'
+    driver_options.add_argument('--headless')
+    browser.config.driver = webdriver.Firefox(options=driver_options)
